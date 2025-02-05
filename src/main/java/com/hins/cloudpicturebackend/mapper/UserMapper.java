467f5df3375120da788e3090133648f859bf9c43
HinsCoder <860845582@qq.com>
2025-02-05 21:07:14 +0800

    User module backend development

---
+++ src/main/java/com/hins/cloudpicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.hins.cloudpicturebackend.mapper;
+
+import com.hins.cloudpicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Hins
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-02-03 22:39:29
+* @Entity com.hins.cloudpicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
